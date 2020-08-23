23fff4f71af266d9e9be5a893e2f8f2d94709c3e
GabeGu <guguibin1988@gmail.com>
2020-08-23 17:14:27 +0800

    提交两数之和的哈希表解法Java版

--- Week_02/001_twoSum/TwoSumMain.java
+++ Week_02/001_twoSum/TwoSumMain.java
@@ -1,11 +1,11 @@
 public class TwoSumMain {
     public static void main(String[] args) {
         TwoSum twoSum = new TwoSum();
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
+        int[] nums = {3, 3};
+        int target = 6;
         int[] result = new int[2];
 
-        result = twoSum.solution1(nums, target);
+        result = twoSum.solution3(nums, target);
         for (int t : result) {
             System.out.println(t);
         }
