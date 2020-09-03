ab6ccdb49b98ab4e809f3cbd255a5c0b208c6f57
GabeGu <guguibin1988@gmail.com>
2020-09-03 22:42:46 +0800

    提交柠檬水找零题解

---
+++ Week_04/860_lemonadeChange/LemonadeChangeMain.java
@@ -0,0 +1,6 @@
+public class LemonadeChangeMain {
+    public static void main(String[] args) {
+        int[] bills = {5, 5, 5, 5, 10, 20, 10};
+        System.out.println(LemonadeChange.solution(bills));
+    }    
+}
