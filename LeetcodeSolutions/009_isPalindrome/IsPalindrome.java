3e654fccc3d52818049bba0dd3436deed3ccb0bb
GabeGu <guguibin1988@gmail.com>
2020-10-09 17:34:37 +0800

    提交回文数题解文件

---
+++ LeetcodeSolutions/009_isPalindrome/IsPalindrome.java
@@ -0,0 +1,5 @@
+public class IsPalindrome {
+    public static boolean solution(int x) {
+        return true;
+    }
+}
---
+++ LeetcodeSolutions/009_isPalindrome/IsPalindromeMain.java
@@ -0,0 +1,6 @@
+public class IsPalindromeMain {
+    public static void main(String[] args) {
+        int x = 11;
+        System.out.println(IsPalindrome.solution(x));
+    }
+}
