be563bd94afb98dd66e06180baee54c4a70c01d7
GabeGu <guguibin1988@gmail.com>
2020-10-11 10:43:01 +0800

    提交位1的个数题解

---
+++ Week_08/HammingWeightMain.java
@@ -0,0 +1,7 @@
+public class HammingWeightMain {
+    public static void main(String[] args) {
+        int x = 0x7FFFFFFF;
+        System.out.println(HammingWeight.solution2(x));
+    }    
+    
+}
