eea7bbc85a33ed8e3614f80ab174b9cdcb775d37
GabeGu <guguibin1988@gmail.com>
2020-10-11 10:35:43 +0800

    提交位1的个数题解

---
+++ LeetcodeSolutions/191_hammingWeight/HammingWeigth.java
@@ -0,0 +1,33 @@
+public class HammingWeigth {
+    /**
+     * 循环右移计数
+     * 时间复杂度：固定循环32次(有局限：运行环境的Int类型是32位)
+     * @param n
+     * @return
+     */
+    public static int solution1(int n) {
+        int cnt = 0;
+
+        for (int shift = 0; shift < 32; shift++) {
+            if ((n & 1) == 1) cnt++;
+            n >>= 1;
+        }
+        return cnt;
+    }
+
+    /**
+     * 利用x&(x-1)来循环消除最低位1直到n为0
+     * 时间复杂度：整数二进制1的个数次
+     * @param n
+     * @return
+     */
+    public static int solution2(int n) {
+        int cnt = 0;
+
+        while (n != 0) {
+            cnt++;
+            n &= n - 1;
+        }
+        return cnt;
+    }
+}
---
+++ LeetcodeSolutions/191_hammingWeight/HammingWeigthMain.java
@@ -0,0 +1,6 @@
+public class HammingWeigthMain {
+    public static void main(String[] args) {
+        int x = 0x7FFFFFFF;
+        System.out.println(HammingWeigth.solution2(x));
+    }    
+}
