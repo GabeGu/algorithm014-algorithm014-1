79149f5ea79a2702b442cbfc4324137db8a412e8
GabeGu <guguibin1988@gmail.com>
2020-10-18 22:32:44 +0800

    提交字符串中的第一个唯一字符题解

---
+++ Week_09/387_firstUniqChar/FirstUniqCharMain.java
@@ -0,0 +1,6 @@
+public class FirstUniqCharMain {
+    public static void main(String[] args) {
+        String s = "loveleetcode";
+        System.out.println(FirstUniqChar.solution(s));
+    }    
+}
